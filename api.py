9f60064b204d605aab27fda81b45229be97bf5d3
U4I5 <u.soglondey@gmail.com>
2021-12-09 12:24:07 +0100

    Upload

---
+++ api.py
@@ -0,0 +1,34 @@
+#Bot make by U4I5 | Github
+
+#Module importation 
+import os
+import time
+import random
+import schedule
+from wallhaven.api import Wallhaven
+
+# set your api here 
+wallhaven = Wallhaven(api_key="bUigG5nroWdX1S2MKRTZDi7Y2diz3Tcr")
+
+# the fonction to download Wallpaper and all other settings
+def download():
+  pass
+  wallhaven.params["sorting"] = "views"
+  wallhaven.params["categories"] = "100"
+  wallhaven.params["atleast"] = "1920x1080"
+  wallhaven.params["ratios"] = '16x9,16x10'
+  wallhaven.params["page"] = random.randint (1,450)
+  results = wallhaven.search()
+  for wallpaper in results.data:
+      wallpaper.save(os.path.dirname('/home/dark/Pictures/Wall Data/'))
+  
+# For repeat process every 4 hours or time you can set
+
+#schedule.every(1).hour.do(download())
+schedule.every(10).minutes.do(download())
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
+
